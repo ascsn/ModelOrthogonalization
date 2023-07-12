8e520de27cc520b97a37097a528ceab06c57250e
pabggpnMSU <78561223+pabggpnMSU@users.noreply.github.com>
2023-07-12 15:09:01 +0000

    Added the true model

--- ToyExample/toymodels.py
+++ ToyExample/toymodels.py
@@ -1,2 +1,17 @@
 import numpy as np
 
+
+def LDM(params,x):
+    #x = (n,z)
+    #params= parameters (volume, surface, asymmetry, Coulomb)
+    n=x[0]
+    z=x[1]
+
+    return params[0]*(n+z) - params[1]*(n+z)**(2/3) - params[2]*((n-z)**2/(n+z)) - params[3]*((z**2)/((n+z)**(1/3)))
+
+LDM_truth_params=[14,13.3,0.57,17]
+
+def Truth(x):
+    return LDM(LDM_truth_params,x)
+
+
