2c85b0748f67e9499ebc75a358c86268b87b56f1
Vojtech Kejzlar <kejzlarv@stt.msu.edu>
2023-08-02 13:48:27 -0400

    Basic PCA analysis of BE and TwoPSE

---
+++ PCAutil/utilities.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def loading_matrix(loadings, n_features, feature_names):
+
+    # PC names
+    pc_list = [f'PC{i}' for i in list(range(1, n_features + 1))]
+
+    # Match PC names to loadings
+    pc_loadings = dict(zip(pc_list, loadings))
+
+    # Matrix of corr coefs between feature names and PCs
+    loadings_df = pd.DataFrame.from_dict(pc_loadings)
+    loadings_df['feature_names'] = feature_names
+    loadings_df = loadings_df.set_index('feature_names')
+    return loadings_df
+
+def biplot(score,coef,labels=None):
+ 
+    xs = score[:,0]
+    ys = score[:,1]
+    n = coef.shape[0]
+    scalex = 1.0/(xs.max() - xs.min())
+    scaley = 1.0/(ys.max() - ys.min())
+    plt.scatter(xs * scalex,ys * scaley, s=5, color='orange')
+ 
+    for i in range(n):
+        plt.arrow(0, 0, coef[i,0], 
+                  coef[i,1],color = 'purple',
+                  alpha = 0.5)
+        plt.text(coef[i,0]* 1.15, 
+                 coef[i,1] * 1.15, 
+                 labels[i], 
+                 color = 'darkblue', 
+                 ha = 'center', 
+                 va = 'center')
+ 
+    plt.xlabel("PC{}".format(1))
+    plt.ylabel("PC{}".format(2))    
+    plt.show()
+    return
--- data/dataLoader.py
+++ data/dataLoader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def h5_data_loader(models, observable, residuals = False):
     """Loads selected_data.h5, and creates a DataFrame with theoretical values of observables + exp values
     
@@ -11,17 +12,18 @@ def h5_data_loader(models, observable, residuals = False):
     Output: Pandas.DataFrame with theoretical vales, experimental values, and (Z,N) pairs
     """
     # Load ZN + BMEX data
-    ZN = pd.read_csv('ZN.dat', delim_whitespace=True)
-    model_data = pd.read_hdf("selected_data.h5", key = 'AME2020', mode = 'r')
+    ZN = pd.read_csv('.\data\ZN.dat', delim_whitespace=True)
+    model_data = pd.read_hdf(".\data\selected_data.h5", key = 'AME2020', mode = 'r')
     
     # Which ZN from ZN.dat are in selected_Data.h5
+    data_dict = {}
     index = model_data[["Z","N"]].isin(ZN).sum(axis = 1) == 2
     data_dict["exp"] = model_data[index][observable].values
     data_pd = pd.DataFrame(data_dict)
 
     # Populating data_pd
     for model in models:
-        model_data = pd.read_hdf("selected_data.h5", key = model, mode = 'r')
+        model_data = pd.read_hdf(".\data\selected_data.h5", key = model, mode = 'r')
 
         index_list = []
         ZN[model] = False
@@ -33,7 +35,7 @@ def h5_data_loader(models, observable, residuals = False):
             if((model_data['Z'] == Z) & (model_data['N'] == N)).any():
                 index_list = index_list + [index]
                 ZN.loc[index,model] = True
-                data_pd.loc[index,model] = model_data.loc[index, observable]
+                data_pd.loc[index,model] = model_data[(model_data['Z'] == Z) & (model_data['N'] == N)][observable].values
 
     data_pd = data_pd.loc[ZN.all(axis = 1),:]
     data_pd["Z"] = ZN.loc[ZN.all(axis = 1),"Z"]
